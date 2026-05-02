7bfab85b9b01e376d74541c9effbcb04a7793b58
FROSTYTRIX <frostytrix13@gmail.com>
2026-05-02 20:15:42 +0200

    Fixed Invisible Steam Box when placed in the world

--- src/main/java/net/frostytrix/fletcherstrestle/block/custom/SteamBoxBlock.java
+++ src/main/java/net/frostytrix/fletcherstrestle/block/custom/SteamBoxBlock.java
@@ -16,6 +16,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -46,13 +47,18 @@ public class SteamBoxBlock extends BaseEntityBlock {
     }
 
     @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return null;
+    }
+
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(WATER_LEVEL);
     }
 
     @Override
-    protected MapCodec<? extends BaseEntityBlock> codec() {
-        return null;
+    protected RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 
     @Nullable
